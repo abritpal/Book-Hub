28a4aeed8e3209018c22f32edd6e2dd21ce0e381
abritpal <abritpal@gmail.com>
2019-10-02 11:36:00 +0530

    first commit

---
+++ src/Book_manager/Goodsreads_clone.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Book_manager;
+
+/**
+ *
+ * @author Abrit
+ */
+public class Goodsreads_clone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
